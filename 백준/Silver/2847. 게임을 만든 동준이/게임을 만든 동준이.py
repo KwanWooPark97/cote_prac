be3d61b8f3ac01bd7fa4d3da948d91b2721fca7d
kwanwoopark <107847933+KwanWooPark97@users.noreply.github.com>
2024-07-31 17:50:05 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+score=[]
+
+for _ in range(n):
+    score.append(int(input()))
+
+score=score[::-1]
+answer=0
+for i in range(1,n):
+    if score[i]>=score[i-1]:
+        answer+=score[i]-(score[i-1]-1)
+        score[i]=score[i-1]-1
+print(answer)
